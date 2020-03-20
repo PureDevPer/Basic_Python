5e0a6c6753a5265222e47d622f6f5c42a37f016f
PureDevPer <wooseok.kim1207@gmail.com>
2020-03-19 22:31:31 -0400

    If, elif, else

---
+++ Conditions/if-else.py
@@ -0,0 +1,40 @@
+money = 200
+watch = 1
+if money >= 300 or watch:
+    print("Take a bus")
+else:
+    print("Walk!")
+
+# Take a bus
+
+if money >= 300 and watch:
+    print("Take a bus")
+else:
+    print("Walk!")
+
+# Walk!
+
+if not watch:
+    print("Take a bus")
+else:
+    print("Walk!")
+
+# Walk!
+
+pocket = ["paper", "headphone", "money"]
+if "money" in pocket:
+    print("Take a bus")
+else:
+    print("Walk")
+
+# Take a bus
+
+
+pocket = ["paper", "headphone"]
+watch = 1
+if "money" in pocket:
+    print("Take a bus")
+elif watch:
+    print("Take a taxi")
+else:
+    print("Walk")
