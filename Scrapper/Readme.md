20ead4ade673b183266613209f550ebfafb354fe
PureDevPer <wooseok.kim1207@gmail.com>
2020-04-27 22:54:16 -0400

    Extracting Indeed Pages

---
+++ Scrapper/Readme.md
@@ -0,0 +1,6 @@
+# Scrapper
+
+## Reference
+
+- https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+- https://github.com/psf/requests
---
+++ Scrapper/main.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+indeed_results = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
+
+indeed_soup = BeautifulSoup(indeed_results.text, "html.parser")
+
+pagination = indeed_soup.find("div", {"class": "pagination"})
+
+pages = pagination.find_all("a")
+spans = []
+for page in pages:
+    spans.append(page.find("span"))
+
+spans = spans[:-1]
+print(spans)
