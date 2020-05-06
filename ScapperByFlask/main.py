fd385f013b106cd8fbaf495d55df323b1dc3a8ef
PureDevPer <wooseok.kim1207@gmail.com>
2020-05-05 22:49:44 -0400

    Server by Flask

---
+++ ScapperByFlask/main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask("SuperScrapper")
+
+
+@app.route("/")
+def home():
+    return "Hello World!"
+
+
+@app.route("/contact")
+def contact():
+    return "Contact me"
+
+
+app.run(host="0.0.0.0")
