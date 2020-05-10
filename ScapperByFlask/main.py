a1b2b4d96b359fdac1dad13c5fe5656094da9aee
PureDevPer <wooseok.kim1207@gmail.com>
2020-05-10 00:12:38 -0400

    Export Route

--- ScapperByFlask/main.py
+++ ScapperByFlask/main.py
@@ -28,6 +28,21 @@ def report():
                            jobs=jobs)
 
 
+@app.route("/export")
+def export():
+    try:
+        word = request.args.get('word')
+        if not word:
+            raise Exception()
+        word = word.lower()
+        jobs = db.get(word)
+        if not jobs:
+            raise Exception()
+        return f"Generate CSV for {word}"
+    except:
+        return redirect("/")
+
+
 @app.route("/<username>")
 def contact(username):
     return f"Hello! I am {username}"
